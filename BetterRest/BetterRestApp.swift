ec9c79a5ae745c97e27d4d187cba4d8cc86490ff
Lukasz Bigos <luki.bigos@gmail.com>
2026-03-04 06:41:42 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Luki on 04/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
